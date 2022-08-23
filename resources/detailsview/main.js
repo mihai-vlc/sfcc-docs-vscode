895b0469542662918baae8b83f2021267e869763
Mihai Ionut Vilcu <ionutvmi@gmail.com>
2022-08-23 23:53:54 +0300

    Simplifies the get selection code

--- resources/detailsview/main.js
+++ resources/detailsview/main.js
@@ -106,19 +106,9 @@
     });
 
     document.addEventListener("selectionchange", () => {
-        const text = getSelectionText();
+        const text = (window.getSelection() || "").toString();
         if (text) {
             lastSelection = text;
         }
     });
-
-    function getSelectionText() {
-        var text = "";
-        if (window.getSelection) {
-            text = (window.getSelection() || "").toString();
-        } else if (document.selection && document.selection.type !== "Control") {
-            text = document.selection.createRange().text;
-        }
-        return text;
-    }
 })();
